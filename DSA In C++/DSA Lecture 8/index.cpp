9b4a84fa01ca451523c04701e17718924af0b99a
Bishwasa <giribiswas415@gmail.com>
2025-03-06 20:28:32 +0530

    Done

---
+++ DSA In C++/DSA Lecture 8/index.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Switch Case
+// int num =1;
+//     switch (num)
+//     {
+//     case 1:
+//         cout << "First"<<endl;
+//         break;
+//     case 2:
+//         cout << "Second"<<endl;
+//         break;
+
+//     default: cout << "Default";
+//         break;
+//     }
+
+
+// exit() in cpp
+// int num =1;
+// while (1)
+// {
+//    switch (num)
+//    {
+//    case 1:cout<<"Hello";
+   
+//     break;
+   
+//    default:
+//     break;
+//    }
+//    exit(1);
+// }
+
+
+// Calculator Using Switch Case
+// int a,b;
+// char c;
+// cin>>a;
+// cin>>b;
+// cin>>c;
+// switch (c)
+// {
+// case '+':
+//     cout<<a+b;
+//     break;
+// case '-':
+//     cout<<a-b;
+//     break;
+// case '*':
+//     cout<<a*b;
+//     break;
+// case '/':
+//     cout<<a/b;
+//     break;
+
+// default:
+//     break;
+// }
+
+
+
+
+
+
+    return 0;
+}
