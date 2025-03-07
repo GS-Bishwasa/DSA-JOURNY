5ad1bc4cbe68fd8d9546e6930c15fc5728d32570
Bishwasa <giribiswas415@gmail.com>
2025-03-07 21:53:04 +0530

    Done

--- DSA In C++/DSA Lecture 8/index.cpp
+++ DSA In C++/DSA Lecture 8/index.cpp
@@ -63,6 +63,91 @@ int main()
 
 
 
+// H.w
+    // int amount;
+    // cout<<"Enter the Amount : "<<endl;
+    // cin>>amount;
+
+    // int num = 1;
+    // int note = 0;
+    // switch (num){
+    //     case 1: {
+    //         note = amount/100;
+    //         amount = amount - (100 * note);
+    //         cout<<note<<" Rs.100 note requied."<<endl;
+    //     }
+    //     case 2:{
+    //         note = amount/50;
+    //         amount = amount - (50 * note);
+    //         cout<<note<<" Rs.50 note requied."<<endl;
+    //     }
+    //     case 3:{
+    //         note = amount/20;
+    //         amount = amount - (20 * note);
+    //         cout<<note<<" Rs.20 note requied."<<endl;
+    //     }
+    //     case 4:{
+    //         note = amount/1;
+    //         amount = amount - (1 * note);
+    //         cout<<note<<" Rs.1 note requied."<<endl;
+    //     }
+    // }
+
+
+    // H.W 2
+    // int n;
+    // cin>>n;
+
+    // int ap= (3*n)+7;
+    // cout<<ap;
+
+
+
+    // H.W 3
+// int a,b;
+// cin>>a;
+// cin>>b;
+// int ans = 0;
+
+// while (a!=0)
+// {
+//     int bit = a%2;
+//     // int bit = a&1;
+//     if (bit==1)
+//     {
+//         ans=ans+1;
+//     }
+    
+//     a>>=1;
+// }
+// while (b!=0)
+// {
+//     int bit = b%2;
+//     // int bit = a&1;
+//     if (bit==1)
+//     {
+//         ans=ans+1;
+//     }
+    
+//     b>>=1;
+// }
+// cout<<ans;
+
+
+
+// H.W 4
+int n;
+cin>>n;
+int num=1;
+int prev=0;
+int temp;
+for (int i = 1; i <= n; i++)
+{
+    temp=num;
+    num=num+prev;
+    prev=temp ;
+}
+cout<<num;
 
 
     return 0;
