7b267bdbe6651c8bdfaeaa70007849630d02c4e3
Bishwasa <giribiswas415@gmail.com>
2025-03-19 20:29:28 +0530

    Done

---
+++ DSA In C++/DSA Lecture 17/BubbleSort.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void BubbleSort(int arr[],int n){
+for (int i = 0; i < n-1; i++)
+{
+    for (int j = 0; j < n-i-1; j++)
+    {
+        if (arr[j]>arr[j+1])
+        {
+            swap(arr[j],arr[j+1]);
+        }
+        
+    }
+    
+}
+for (int i = 0; i < n; i++)
+{
+    cout<<arr[i]<<" ";
+}
+
+}
+
+int main(){
+int arr[6]={10,1,7,6,14,9};
+BubbleSort(arr,6);
+return 0;
+}
