7aef7c7ae8ff0c7159dfedb58091e225a1916c72
Bishwasa <giribiswas415@gmail.com>
2025-03-28 20:58:50 +0530

    Done

---
+++ DSA In C++/DSA Lecture 21/CheckifArrayIsSortedandRotated.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int checkarr(vector<int> nums)
+{
+    int count = 0;
+    int n = nums.size();
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i - 1] > nums[i])
+        {
+            count++;
+        }
+    }
+    if (nums[n - 1] > nums[0])
+    {
+        count++;
+    }
+    return count <= 1;
+}
+
+int main()
+{
+
+    vector<int> arr;
+    arr.push_back(3);
+    arr.push_back(4);
+    arr.push_back(5);
+    arr.push_back(1);
+    arr.push_back(2);
+
+    cout<<checkarr(arr);
+    return 0;
+}
