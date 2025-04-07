c2af31502dced63ac6d262312df30bd234f21027
Bishwasa <giribiswas415@gmail.com>
2025-04-07 15:05:01 +0530

    Done

--- DSA In C++/DSA Lecture 23/2D-Array.cpp
+++ DSA In C++/DSA Lecture 23/2D-Array.cpp
@@ -1,7 +1,75 @@
 #include<iostream>
 using namespace std;
+
+
+bool isPresent(int arr[][3], int target, int row, int col) {
+
+    for(int row=0; row<3; row++) {
+          for(int col=0; col<3; col++) {
+              
+              if( arr[row][col] == target) {
+                  return 1;
+              }
+              
+          }
+      }
+      return 0;
+  }
+
+
+  void printColSum(int arr[][3], int row, int col) {
+    cout << "Printing Sum -> " << endl;
+    for(int col=0; col<3; col++) {
+        int sum = 0;
+        for(int row=0; row<3; row++) {
+            sum += arr[row][col];
+        }
+        cout << sum << " ";
+    }
+
+    cout << endl;
+}
+
 int main(){
-int arr[3][3] = {1,2,3,4,5,6,7,8,9};
-cout<<arr[0][7];
+int arr[3][3];
+// int arr[3][3] = {1,2,3,4,5,6,7,8,9};
+// int arr[3][3] = {{1,11,111},{2,22,222},{3,33,333}};
+
+
+// for (int i = 0; i < 3; i++)
+// {
+//     for (int j = 0; j < 3; j++)
+//     {
+//         cin>> arr[i][j];
+//     }
+    
+// }
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        cout<< arr[i][j]<<" ";
+    }
+    cout<<endl;
+    
+}
+
+
+
+    cout <<" Enter the element to search " << endl;
+    int target;
+    cin >> target;
+
+    if(isPresent(arr, target, 3, 3)) {
+        cout <<" Element found " << endl;
+    }
+    else{
+        cout <<" Not Found" << endl;
+    }
+
+    printColSum(arr, 3, 3 );
+    
+
 return 0;
 }
