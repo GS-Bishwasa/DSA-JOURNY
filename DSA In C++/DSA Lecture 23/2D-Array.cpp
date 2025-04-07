2c93b2760f8defc94ce98af8f0543e5fd49d7251
Bishwasa <giribiswas415@gmail.com>
2025-04-07 18:04:03 +0530

    Done

--- DSA In C++/DSA Lecture 23/2D-Array.cpp
+++ DSA In C++/DSA Lecture 23/2D-Array.cpp
@@ -30,9 +30,34 @@ bool isPresent(int arr[][3], int target, int row, int col) {
     cout << endl;
 }
 
+
+
+int largestRowSum(int arr[][3], int row, int col) {
+
+    int maxi = INT_MIN;
+    int rowIndex = -1;
+
+    for(int row=0; row<3; row++) {
+        int sum = 0;
+        for(int col=0; col<3; col++) {
+            sum += arr[row][col];
+        }
+
+        if(sum > maxi ) {
+            maxi = sum;
+            rowIndex = row;
+        }
+       
+    }
+
+    cout << "the maximum sum is " << maxi << endl;
+    return rowIndex;
+}
+
+
 int main(){
-int arr[3][3];
-// int arr[3][3] = {1,2,3,4,5,6,7,8,9};
+// int arr[3][3];
+int arr[3][3] = {1,2,3,4,5,6,7,8,9};
 // int arr[3][3] = {{1,11,111},{2,22,222},{3,33,333}};
 
 
@@ -57,19 +82,22 @@ for (int i = 0; i < 3; i++)
 
 
 
-    cout <<" Enter the element to search " << endl;
-    int target;
-    cin >> target;
+    // cout <<" Enter the element to search " << endl;
+    // int target;
+    // cin >> target;
 
-    if(isPresent(arr, target, 3, 3)) {
-        cout <<" Element found " << endl;
-    }
-    else{
-        cout <<" Not Found" << endl;
-    }
+    // if(isPresent(arr, target, 3, 3)) {
+    //     cout <<" Element found " << endl;
+    // }
+    // else{
+    //     cout <<" Not Found" << endl;
+    // }
 
-    printColSum(arr, 3, 3 );
+    // printColSum(arr, 3, 3 );
     
+    int ansIndex = largestRowSum(arr,3,3);
+    cout << " Max row is at index " << ansIndex << endl;
+
 
 return 0;
 }
