40586fe59a6bd35c4ad9375fb457e87a88d1a4ce
Bishwasa <giribiswas415@gmail.com>
2025-04-14 21:22:17 +0530

    Done

---
+++ DSA In C++/DSA Lecture 25/Pointers-In-C++.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int num = 5;
+// cout<<num;
+
+
+// Address of Operator --> &  (Isse aap variable ka address dekh sakte hain, Yani Kis Address pe variable Mapped Hai)
+cout<<"Address Of num is ->"<<&num;
+
+
+return 0;
+}
