dcd1d926b6a084891d398d349b9d3bc36ce5d85a
Bishwasa <giribiswas415@gmail.com>
2025-04-15 21:52:45 +0530

    Done

--- DSA In C++/DSA Lecture 25/Pointers-In-C++.cpp
+++ DSA In C++/DSA Lecture 25/Pointers-In-C++.cpp
@@ -1,14 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 
-int num = 5;
-// cout<<num;
+    int num = 5;
+    char ch = 'a';
+    double d = 15.7;
+    // cout<<num;
 
+    // Address of Operator --> &  (Isse aap variable ka address dekh sakte hain, Yani Kis Address pe variable Mapped Hai)
+    cout << "Address Of num is ->" << &num << endl;
 
-// Address of Operator --> &  (Isse aap variable ka address dekh sakte hain, Yani Kis Address pe variable Mapped Hai)
-cout<<"Address Of num is ->"<<&num;
+    int *ptr = &num;
+    // OR
+    int *q = 0;
+    q = &num;
 
+    char *p = &ch;
+    double *dbl = &d;
+    // Here * Is Dereference Operator
+    cout << *ptr << endl;
+    cout << *q << endl;
+    cout << *p << endl;
+    cout << *dbl << endl;
 
-return 0;
+    // Copying a Pointer
+    int *r = q;
+    cout << endl
+         << "Copying a Pointer -->" << *r << endl;
+
+    // Pointer Arithmatic
+    int *t = &num;
+    cout<<endl << *t << endl;
+    cout << "Before t=t+1; -->" << t << endl;
+    t = t + 1;
+    cout << "After t=t+1; -->" << t << endl;
+
+    return 0;
 }
