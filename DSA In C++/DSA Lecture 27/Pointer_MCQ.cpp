779da0fb360b83e87a1a4397bae36c855320e9c2
Bishwasa <giribiswas415@gmail.com>
2025-04-21 21:10:42 +0530

    Lecture27: Double Pointers in C++ (10 Pointer Short MCQ Type Questions Practice)

---
+++ DSA In C++/DSA Lecture 27/Pointer_MCQ.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // 1st Question
+    // int first = 8;
+    // int second = 18;
+    // int *ptr = &second;
+    // *ptr = 9;
+    // cout  << first << " " << second << endl;
+
+    // 2nd Question
+    // int first = 6;
+    // int *p = &first;
+    // int *q = p;
+    // (*q)++;
+    // cout << first  << endl;
+
+    // 3rd Question
+    // int first = 8;
+    // int *p = &first;
+    // cout << (*p)++ << " ";
+    // cout << first << endl;
+
+    // 4th Question
+    // int *p = 0;
+    // int first = 110;
+    // p = &first;
+    // cout << *p << endl;
+
+    // 5th Question
+    // int first = 8;
+    // int second = 11;
+    // int *third = &second; 
+    // first = *third;
+    // *third = *third + 2;
+    // cout  << first << "  " << second << endl;
+
+    // 6th Question
+    // float f = 12.5;
+    // float p = 21.5;
+    // float* ptr = &f;
+    // (*ptr)++;
+    // *ptr = p;
+    // cout << *ptr << " " << f << " " << p << endl;
+
+    // 7th Question
+    // int arr[5];
+    // int *ptr;
+    // cout << sizeof(arr) << " " << sizeof(ptr) << endl;
+
+    // 8th Question
+    // int arr[] = {11, 21, 13, 14};
+    // cout << *(arr) << " " << *(arr+1) << endl;
+
+    // 9th Question
+    // int arr[6] = {11, 12, 31};
+    // cout << arr << " " << &arr << endl;
+
+    // 10th Question
+    // int arr[6] = {11, 21, 13};
+    // cout << (arr + 1) << endl;
+
+    
+
+    return 0;
+}
