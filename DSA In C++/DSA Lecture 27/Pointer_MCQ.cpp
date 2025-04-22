09b931e2a4f1b8b76401f1fffd248cf92c8529b3
Bishwasa <giribiswas415@gmail.com>
2025-04-22 20:59:39 +0530

    Lecture27: Double Pointers in C++ (More Pointer Short MCQ Type Questions Practice)

--- DSA In C++/DSA Lecture 27/Pointer_MCQ.cpp
+++ DSA In C++/DSA Lecture 27/Pointer_MCQ.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 using namespace std;
+
+// 10th Question
+// void update(int *p){
+//     *p = (*p)  *  2;
+// }
+
+// 19th Question
+// void fun(int arr[]) {
+//     cout << arr[0] << " ";
+// }
+
+// 20th Question
+// void square(int *p){
+//     int a = 70;
+//     p = &a;
+//     *p = (*p) * (*p);
+// }
+
 int main()
 {
     // 1st Question
@@ -31,7 +49,7 @@ int main()
     // 5th Question
     // int first = 8;
     // int second = 11;
-    // int *third = &second; 
+    // int *third = &second;
     // first = *third;
     // *third = *third + 2;
     // cout  << first << "  " << second << endl;
@@ -61,7 +79,85 @@ int main()
     // int arr[6] = {11, 21, 13};
     // cout << (arr + 1) << endl;
 
-    
+    // 11th Question
+    // int arr[6] = {11, 21, 31};
+    // int *p = arr;
+    // cout << p[2] << endl;
+
+    // 12th Question
+    // int arr[] = {11, 12, 13, 14, 15};
+    // cout << *(arr) << " " << *(arr + 3);
+
+    // 13th Question
+    int arr[] = {11, 21, 31, 41};
+    int *ptr = arr++;
+    cout << *ptr << endl;
+
+    // 14th Question
+    //  char ch = 'a';
+    // char* ptr = &ch;
+    // ch++;
+    // cout << *ptr << endl;
+
+    // 15th Question
+    // char arr[] = "abcde";
+    // char *p = &arr[0];
+    // cout << p << endl;
+
+    // 16th Question
+    // char arr[] = "abcde";
+    // char *p = &arr[0];
+    // p++;
+    // cout << p << endl;
+
+    // 17th Question
+    // char str[]= "babbar";
+    // char *p = str;
+    // cout << str[0] << " " << p[0] << endl;
+
+    // 18th Question
+    //     int i = 10;
+    //     update(&i);
+    //     cout << i << endl;
+    // }
+
+    // 19th Question
+    //     int arr[] = {11, 12, 13, 14};
+    //     fun(arr + 1);
+    //     cout << arr[0] << endl;
+    // }
+
+    // 20th Question
+    // int main(){
+    //     int a = 70;
+    //     square(&a);
+    //     cout << a << endl;
+    // }
+
+    // 21th Question
+    // int first = 10;
+    // int *p = &a;
+    // int **q = &p;
+    // int second = 20;
+    // *q = &second;
+    // (*p)++;
+    // cout << first << " " << second << endl;
+
+    // 22th Question
+    // int first = 110;
+    // int *p = &first;
+    // int **q = &p;
+    // int second = (**q)++ + 9;
+    // cout << first << " " << second << endl;
+
+    // 23th Question
+    // int first = 100;
+    // int *p = &first;
+    // int **q = &p;
+    // int second = ++(**q);
+    // int *r = *q;
+    // ++(*r);
+    // cout << first << " " << second << endl;
 
     return 0;
 }
